59a07e6fc841cfec2c5c63907d5cf065a219b0ac
Angelo Netho <angelonetho@gmail.com>
2024-07-27 22:55:48 -0300

    Finalização da Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,21 @@
-import { HeaderContainer } from "./styled";
+import { HeaderContainer } from './styled'
 
 import logoIgnite from '../../assets/logo-ignite.svg'
-import { Scroll, Timer } from "phosphor-react";
-import { NavLink } from "react-router-dom";
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <img src={logoIgnite} alt="" />
-            <nav>
-                <NavLink to={"/"} title="Timer">
-                    <Timer size={24} />
-                </NavLink>
-                <NavLink to="/history" title="Histórico">
-                    <Scroll size={24} />
-                </NavLink>
-            </nav>
-        </HeaderContainer>
-    )
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to={'/'} title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,45 @@
-import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmoutInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
 
 export function Home() {
-    return (
-        <HomeContainer>
-            <form action="">
-                <FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <input id="task" />
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
-                    <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" id="minutesAmount" />
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmoutInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
 
-                    <span>minutos.</span>
-                </FormContainer>
+          <span>minutos.</span>
+        </FormContainer>
 
-                <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
-                    <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
-                </CountdownContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
 
-                <button type="submit">
-                    <Play size={24} />
-                    Começar
-                </button>
-            </form>
-        </HomeContainer>
-    )
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
